a8feaff4b357a71d126f07ddc8dc97b7fc4a4f28
Bew Harichanwong <bew.hari@gmail.com>
2016-02-20 02:22:05 -0600

    Add iisnode.yml

--- model/db.js
+++ model/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://process.env.DB_USR:process.env.DB_PWD@ds058508.mongolab.com:58508/MongoLabTab');
+mongoose.connect('mongodb://test:hack@ds058508.mongolab.com:58508/MongoLabTab');
--- model/users.js
+++ model/users.js
@@ -2,6 +2,5 @@ var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
-
 });
 mongoose.model('User', userSchema);
